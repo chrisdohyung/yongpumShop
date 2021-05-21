4b8743abe2c7630e06c6b695ea12528cbe392974
chrisdohyung <chris.dohyung@gmail.com>
2021-05-21 16:31:10 +0900

    ..

--- src/com/yongpum/shop/user/UserDao.java
+++ src/com/yongpum/shop/user/UserDao.java
@@ -1,10 +1,23 @@
 package com.yongpum.shop.user;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class UserDao {
 	
 	private DataSource dataSource;
 	
+	public UserDao() throws Exception {
+		Properties properties = new Properties();
+		properties.load(this.getClass().getResourceAsStream("/com/yongpum/shop/db.properties"));
+		BasicDataSource basicDatasource = new BasicDataSource();
+		basicDatasource.setDriverClassName(properties.getProperty("driverClass"));
+		basicDatasource.setUrl(properties.getProperty("url"));
+		basicDatasource.setUserName(properties.getProperty("user"));
+		basicDatasource.setPassword(properties.getProperty("password"));
+		dataSource = basicDataSource;
+	}
 
 }
