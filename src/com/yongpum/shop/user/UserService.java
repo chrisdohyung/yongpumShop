ac9be070bbc3adac8e90a3e44cefb115a2959ed1
chrisdohyung <chris.dohyung@gmail.com>
2021-05-22 10:10:46 +0900

    user not found exception created

--- src/com/yongpum/shop/user/UserService.java
+++ src/com/yongpum/shop/user/UserService.java
@@ -1,5 +1,7 @@
 package com.yongpum.shop.user;
 
+import com.yongpum.shop.user.exception.ExistedUserException;
+
 public class UserService {
 	private UserDao userDao;
 	public UserService() throws Exception {
@@ -8,9 +10,17 @@ public class UserService {
 	
 	public int create(User user) throws Exception {
 		if (userDao.existedUser(user.getUserId())) {
-			throw new ExistedUserException(user.getUserId()+" your id is not existed.");
+			throw new ExistedUserException(user.getUserId()+" your id is already existed.");
 		}
 		return userDao.create(user);
+	}
+	
+	public User login(String userId, String password) throws Exception {
+		User user = userDao.findUser(userId);
+		if (user == null) {
+			throw new UserNotFoundException()userId +" your id is not existed";
+		}
+		return user;
 	}
 
 }
---
+++ src/com/yongpum/shop/user/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.yongpum.shop.user.exception;
+
+public class UserNotFoundException extends Exception {
+	public UserNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
