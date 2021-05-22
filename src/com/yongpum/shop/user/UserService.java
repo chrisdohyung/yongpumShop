137bc3af5efa6a1e40686078bd8d4af2791d58b0
chrisdohyung <chris.dohyung@gmail.com>
2021-05-22 10:02:57 +0900

    ..

--- src/com/yongpum/shop/user/UserService.java
+++ src/com/yongpum/shop/user/UserService.java
@@ -5,5 +5,12 @@ public class UserService {
 	public UserService() throws Exception {
 		userDao = new UserDao();
 	}
+	
+	public int create(User user) throws Exception {
+		if (userDao.existedUser(user.getUserId())) {
+			throw new ExistedUserException(user.getUserId()+" your id is not existed.");
+		}
+		return userDao.create(user);
+	}
 
 }
