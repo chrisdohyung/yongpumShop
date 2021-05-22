8ed149b438cfb4748c5fca9b6ff988e15bb31176
chrisdohyung <chris.dohyung@gmail.com>
2021-05-22 19:51:40 +0900

    userdao test

---
+++ src/com/yongpum/shop/test/UserDaoTestMain.java
@@ -0,0 +1,14 @@
+package com.yongpum.shop.test;
+
+import com.yongpum.shop.user.UserDao;
+
+public class UserDaoTestMain {
+
+	public static void main(String[] args) throws Exception {
+		
+		UserDao userDao = new UserDao();
+		System.out.println(userDao.findUserList());
+		
+	}
+
+}
